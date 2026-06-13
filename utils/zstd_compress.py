25f498d0b3c85179ca3a8533ebd4818974ee5963
ultragreed <agent002004@gmail.com>
2026-06-13 14:00:11 +1000

    Add requirements and change utils: compress dir to compress file

--- utils/zstd_compress.py
+++ utils/zstd_compress.py
@@ -22,22 +22,20 @@ def compress_file(path_in, path_out, level, threads):
     Path(path_out).write_bytes(compressed)
 
 
-def main(dir_in, dir_out):
-    print(f'Compressing data from {dir_in} to {dir_out}')
+def main(file_in, dir_out):
+    print(f'Compressing {file_in} to {dir_out}')
     for level in (pbar := tqdm(LEVELS)):
         pbar.set_description(f'level {level}')
         level_path = Path(dir_out) / str(level)
         level_path.mkdir(exist_ok=True)
 
-        files = sorted(file for file in Path(dir_in).iterdir() if file.is_file())
-        for orig_path in (pbar2 := tqdm(files, leave=False)):
-            pbar2.set_description(f'file {orig_path.name}')
-            compress_path = f'{level_path / orig_path.name}.zst'
-            compress_file(orig_path, compress_path, level, THREADS)
+        orig_path = Path(file_in)
+        compress_path = f'{level_path / orig_path.name}.zst'
+        compress_file(orig_path, compress_path, level, THREADS)
     print('Compression done!')
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
-        print("Usage: gen_zstd.py PATH_IN PATH_OUT")
+        print("Usage: zstd_compress_dir.py PATH_IN_FILE PATH_OUT_DIR")
         exit(2)
     main(*sys.argv[1:])
